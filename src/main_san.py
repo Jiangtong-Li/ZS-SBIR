d2c1503304fcde8ab1d0e7cf950b3e8f1e21d272
Zhixin Ling <1069066484@qq.com>
2019-10-08 12:26:14 +0800

    Update main_san.py

--- src/main_san.py
+++ src/main_san.py
@@ -92,16 +92,17 @@ def _eval(feats_labels_sk, feats_labels_im, n):
     :param n: the top n elements used for evaluation
     :return: precision@n, mAP@n
     """
-    # print(feats_labels_sk[0].shape, feats_labels_sk[1].shape, feats_labels_im[0].shape, feats_labels_im[1].shape)  # (16, 256) (16,) (16, 256) (16,)
+    # print("eval\n", len(feats_labels_sk[1]), len(feats_labels_im[1])) # 1099 581
+
     dists = cdist(feats_labels_sk[0], feats_labels_im[0], 'euclidean')
-    # print("dists.shape=", dists.shape)  # (16, 16)
-    ranks = np.argsort(dists, 0)
-    # print("ranks.shape=", ranks.shape)  # (16, 16)
-    ranksn = ranks[:n, :].T
-    # print("ranksn.shape=", ranksn.shape)  # (16, 3)
+    # print("dists.shape=", dists.shape)  # (1099, 581)
+    ranks = np.argsort(dists, 1) # (1099, 581)
+
+    ranksn = ranks[:, :n] # (1099, 50)
+    # print("dists ranks ranksn shape=", dists.shape, ranks.shape, ranksn.shape)
+
     classesn = np.array([[feats_labels_im[1][i] == feats_labels_sk[1][r] for i in ranksn[r]] for r in range(len(ranksn))])
-    # ("classesn.shape=", classesn.shape)  # (16, 3)
-    # exit(0)
+
     return _get_pre_from_matches(classesn), _get_map_from_matches(classesn)
 
 
@@ -124,7 +125,8 @@ def _check_args_paths(args):
 
 
 def train(args):
-    # srun --gres=gpu --exclusive python main_san.py --steps 50000 --print_every 500 --save_every 2000 --batch_size 64 --dataset sketchy --margin 1 --npy_dir 0
+    # srun -p gpu --gres=gpu:1 --exclusive --output=san1.out python main_san.py --steps 50000 --print_every 500 --save_every 2000 --batch_size 96 --dataset sketchy --margin 1 --npy_dir 0 --san_test1
+
     sketch_folder, image_folder, train_class, test_class = _check_args_paths(args)
 
     data_train = SaN_dataloader(folder_sk=sketch_folder, clss=train_class, folder_nps=args.npy_dir,
@@ -142,9 +144,9 @@ def train(args):
     logger.info(str(args))
     args.steps += steps
     san_loss = _SaN_loss(args.margin)
+    model.train()
     while True:
         loss_sum = []
-        model.train()
         for _, (sketch, positive_image, negative_image, positive_class_id) in enumerate(dataloader_train):
             optimizer.zero_grad()
             loss = san_loss(model(sketch.cuda()),
@@ -172,7 +174,7 @@ def train(args):
                 model.train()
 
             if (steps + 1) % args.print_every == 0:
-                print('step: {},  loss: {}'.format(steps, np.mean(loss_sum)))
+                logger.info('step: {},  loss: {}'.format(steps, np.mean(loss_sum)))
                 loss_sum = []
 
             steps += 1

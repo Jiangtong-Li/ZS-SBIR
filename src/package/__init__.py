756d9780fc0c27aec92ae7852bd69ae3fc232f9d
Jiangtong Li <keep_moving-Lee@sjtu.edu.cn>
2019-10-19 10:33:38 +0800

    cvae-pytorch

--- src/package/__init__.py
+++ src/package/__init__.py
@@ -26,3 +26,20 @@ def cal_matrics(image_f1, image_f2, image_l, sketch_f1, sketch_f2, sketch_l, lam
         if mAP > mAP_b:
             mAP_b = mAP
     return precision_b, mAP_b, lambda_b
+
+def cal_matrics_single(image_f, image_l, sketch_f, sketch_l, n=200):
+
+    dists = cdist(image_f, sketch_f, 'cosine')
+    precision_b = 0
+    mAP_b = 0.
+    rank = np.argsort(dists, 0)
+    ranksn = rank[:n, :].T
+    classesn = np.array([[image_l[i] == sketch_l[r] for i in ranksn[r]] for r in range(len(ranksn))]) # ske_size*n
+    precision = np.mean(classesn)
+    # Cal MAP
+    """
+    Test case: np.array([[1,0,1,0,0,1,0,0,1,1],[0,1,0,0,1,0,1,0,0,0]])
+    Answer: 0.53
+    """
+    mAP = np.mean(np.sum(classesn*np.cumsum(classesn, axis=1)/np.cumsum(np.ones(classesn.shape), axis=1), axis=1)/n)
+    return precision, mAP

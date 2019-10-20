301919015987dea29b7a597e8481ab4b13690858
Jiangtong-Li <jiangtongli1997@gmail.com>
2019-10-20 11:47:32 +0800

    cmtrans

--- script/run_cmtrans_gpu2-2.sh
+++ script/run_cmtrans_gpu2-2.sh
@@ -4,14 +4,14 @@ python ./src/main_cmdtrans.py  --sketch_dir ./data/256x256/sketch/tx_00010000000
                                --packed_pkl_nozs ./data/preprocessed/cm_trans_sketch_all_unpair/nozs_packed.pkl \
                                --packed_pkl_zs ./data/preprocessed/cm_trans_sketch_all_unpair/zs_packed.pkl \
                                --embedding_file ./data/GoogleNews-vectors-negative300.bin \
-                               --preprocess_data ./data/preprocessed/cm_trans_sketch_all_unpair/CNN_feature_2048.h5py \
+                               --preprocess_data ./data/preprocessed/cm_trans_sketch_all_unpair/CNN_feature_1024.h5py \
                                --raw_data ./data/preprocessed/cm_trans_sketch_all_unpair/CNN_feature_5568.h5py \
                                --log_file ./log/cmd_trans_gpu2-2.log \
                                --shuffle \
                                --sample_times 10 \
-                               --pca_size 2048 \
+                               --pca_size 1024 \
                                --raw_size 5568 \
-                               --hidden_size 2048 \
+                               --hidden_size 1024 \
                                --semantics_size 300 \
                                --fix_embedding \
                                --seman_dist l2 \
